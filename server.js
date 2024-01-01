4e8b3a27e56fb4d193d2bd1b3be4a00a5fc0e715
Umeno0923 <ihsasum0923@icloud.com>
2024-01-02 03:20:00 +0900

    ローカルホストサーバー構築

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-console.log("modejs");
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const io = require("socket.io")(server);
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("chatApp");
+});
+
+server.listen(PORT, () => {
+  console.log("listening on 3000");
+});
