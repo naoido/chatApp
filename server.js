db4902c19764865158276bae55d8425cd98fcb14
なおいど <sentyouido@gmail.com>
2024-01-09 10:13:51 +0900

    Refactor: 修正

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-const express = require("express");
-const app = express();
-const http = require("http");
-const server = http.createServer(app);
-const io = require("socket.io")(server);
-const PORT = 3000;
+const express =  require("express");
+const http    =  require("http");
+const io      =  require("socket.io")(server);
+
+const app     =  express();
+const server  =  http.createServer(app);
+const PORT    =  3000;
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
